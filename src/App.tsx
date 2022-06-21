3f38491f8e03cd0e3b734fb7cb1156a2da6e9442
Alexander Wansiedler <contact@wansiedler.com>
2022-06-21 12:12:55 +0200

    codility

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {ReactElement, useState} from 'react';
 import './App.css';
 
 function checkNumber(input) {
@@ -22,9 +22,70 @@ function checkNumber(input) {
 	return a
 }
 
+type MenuItem = {
+	title: string;
+	subItems?: Array<string>;
+};
+
+type MenuConfig = Array<MenuItem>;
+
+function Solution({menuConfig}: { menuConfig: MenuConfig }): ReactElement {
+	const [active, setActive] = useState("");
+
+	return <div className="menu-wrapper">
+		{menuConfig.map(
+			({title, subItems}, idx) =>
+				<div data-test-id={"first-level-" + title.toLowerCase()}
+
+				     key={idx}
+				>
+					{title}
+					{
+						subItems && <>
+                            <button
+                                data-test-id={"button-" + title.toLowerCase()}
+                                onClick={() => {
+									setActive(active === title ? "" : title)
+								}}
+                            >
+								{active !== title ? "Expand" : "Hide"}
+                            </button>
+							{active === title ? <ul data-test-id={"ul-" + title.toLowerCase()}>
+								{
+									subItems.map((item, idx) => <li
+										data-test-id={"li-" + title.toLowerCase() + "-" + item.toLowerCase()}
+										key={idx}
+									>{item}</li>)
+								}
+							</ul> : null}
+
+                        </>
+					}
+				</div>,
+		)}
+
+	</div>;
+}
+
 function App() {
-	const a = checkNumber([1, 4, -6, 3, 7, -3, 10])
-	return <>{a}</>
+	// const a = checkNumber([1, 4, -6, 3, 7, -3, 10])
+	return <>
+		<Solution
+			menuConfig={[
+				{
+					title: 'Home',
+				},
+				{
+					title: 'Services',
+					subItems: ['Cooking', 'Cleaning'],
+				},
+				{
+					title: 'Contact',
+					subItems: ['Phone', 'Mail'],
+				},
+			]}
+		/>
+	</>
 }
 
 export default App;
